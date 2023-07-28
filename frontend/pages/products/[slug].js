0f928134e675e07505196e3b57a3879b8260e52c
DevvSakib <devvsakib@gmail.com>
2023-07-29 00:50:00 +0600

    product image loader and back btn added

--- frontend/pages/products/[slug].js
+++ frontend/pages/products/[slug].js
@@ -1,8 +1,11 @@
 import Layout from '@/components';
 import { CartContext } from '@/context/CartProvider';
 import { useProductById } from '@/lib/API';
+import Image from 'next/image';
+import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { useContext } from 'react'
+import { BsArrowLeft } from 'react-icons/bs';
 
 
 const ProductDetail = () => {
@@ -17,16 +20,30 @@ const ProductDetail = () => {
     }
 
     return (
-        <div className='my-32'>
+        <div className='mb-32 mt-12'>
             <Layout>
-                <h1 className='mb-5 bg-clip-text text-transparent bg-gradient-to-tr to-teal-400 from-indigo-600 text-xl md:text-3xl font-bold text-center'>{product?.model}</h1>
+                <Link href="/products">
+                    <div className='flex items-center gap-2'>
+                        <BsArrowLeft />
+                        <span>
+                            Go Back
+                        </span>
+                    </div>
+                </Link>
+                <h1 className='mb-5 mt-10 bg-clip-text text-transparent bg-gradient-to-tr to-teal-400 from-indigo-600 text-xl md:text-3xl font-bold text-center'>{product?.model}</h1>
                 <div className='my-5 mt-10 grid grid-cols-1 md:grid-cols-3'>
-                    <div>
-                        <img
-                            src={product?.image ?? "Loading"}
-                            alt={product?.model ?? "Loading"}
-                            className='mx-auto'
-                        />
+                    <div className='grid place-items-center'>
+                        {
+                            isLoading ? <Image
+                                src={"/spinner.svg"}
+                                alt="Spinner"
+                                className="mx-auto"
+                            /> : <img
+                                src={product?.image}
+                                alt={product?.model ?? "Loading"}
+                                className='mx-auto'
+                            />
+                        }
                     </div>
                     <div className='mx-auto md:w-3/12 mt-5 col-span-2 flex flex-col gap-5'>
                         <p className='flex gap-5'><b>Brand:</b> {product?.brand ?? "Loading"}</p>
